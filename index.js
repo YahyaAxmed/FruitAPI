048fda14759851439414637ec8d3747a68399c42
YahyaAxmed <146835429+YahyaAxmed@users.noreply.github.com>
2025-08-12 09:42:42 +0100

    Add .env file to project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const {app} = require("./app")
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port,()=>{
     console.log(`Fruity API currently running on port ${port}`)
